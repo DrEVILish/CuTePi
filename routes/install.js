c6ebc89fbfd2c249b032ca6482c2f84beb588a5d
Ryan Freeman <ryan.j.freeman@me.com>
2024-12-02 01:15:50 +0000

    Update Install to have updated SQL

--- routes/install.js
+++ routes/install.js
@@ -8,10 +8,15 @@ router.get('/', async (req,res)=>{
 
   const createTableMediapool = `CREATE TABLE mediapool (
     media_id INTEGER PRIMARY KEY NOT NULL,
-    filename TEXT UNIQUE NOT NULL
+    filename TEXT UNIQUE NOT NULL,
+    mimetype TEXT,
+    size INTEGER,
+    duration REAL
     )`;
   const createTableCuesheet = `CREATE TABLE cuesheet (
     cue_id INTEGER PRIMARY KEY NOT NULL,
+    cuePos INTEGER UNIQUE,
+    cueNum TEXT UNIQUE,
     media_id INTEGER NOT NULL,
     title TEXT UNIQUE NOT NULL,
     posStart INTEGER,
